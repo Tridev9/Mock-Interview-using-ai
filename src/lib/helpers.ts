4708ad691c7b0725083e9cbad33211ab591ced5f
Tridev9 <triddev123@gmail.com>
2025-03-01 18:23:34 +0530

    Header component completed

---
+++ src/lib/helpers.ts
@@ -0,0 +1,20 @@
+import { href } from "react-router-dom";
+
+export const MainRoutes=[
+    {
+        label:"Home",
+        href:"/"
+    },
+    {
+        label:"Contact Us",
+        href:"/contact"
+    },
+    {
+        label:"About Us",
+        href:"/about"
+    },
+    {
+        label:"Services",
+        href:"/services"
+    },
+]
