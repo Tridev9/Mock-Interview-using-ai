45ec0046500d15232376657b581d98a1a168f0e1
Tridev9 <triddev123@gmail.com>
2025-03-01 19:23:25 +0530

    firebase confiration & users data saved

--- src/layouts/public-layout.tsx
+++ src/layouts/public-layout.tsx
@@ -1,10 +1,12 @@
 import Header from "@/components/header"
 import Footer from "@/components/footer"
 import { Outlet } from "react-router-dom"
+import AuthHandler from "@/handlers/auth-handler"
 const PublicLayout = () => {
   return (
     <div className="w-full">
       {/* handler to store the user data */}
+      <AuthHandler/>
       <Header/>
       <Outlet/>
       <Footer/>
